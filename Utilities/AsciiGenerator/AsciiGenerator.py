c8f6493ac4980bf0220354fccad07c8176b965d8
Taylor Kuno <kunot@oregonstate.edu>
2022-09-25 19:14:47 -0500

    working but could use improvements, img distorted

--- Utilities/AsciiGenerator/AsciiGenerator.py
+++ Utilities/AsciiGenerator/AsciiGenerator.py
@@ -1,34 +1,65 @@
 # ASCII Cartoon Generator
 # Author: Taylor Kuno
 # Adapted From: https://www.youtube.com/watch?v=v_raWlX7tZY
-# Description:  Takes an image of a drawing and returns a ASCII cartoon 
-#               The cartoon is printed both to the console and to the 
+#               https://github.com/kiteco/python-youtube-code/blob/master/ascii/ascii_convert.py
+# Description:  Takes an image of a drawing and returns a ASCII cartoon
+#               The cartoon is both displayed in console and saved to file
 
-"""
-@ # S %  * +  : 
-"""
+import os
+from PIL import Image
 
-import PIL
+ASCII_CHARS = ["@", "#", "S", "%", "?", "*", "+", " ", " ", " ", " "]
 
 
-def resize_image(image, size):
-    pass
-    
+def resize_image(image, new_width):
+    width, height = image.size
+    new_height = int(new_width * (height/width))
+    resized_image = image.resize((new_width, new_height))
+    return (resized_image)
 
-def main():
-    # Open image
-    # Print error if file ivalid
-    # Create ASCII char list
-    # resize image
-    # convert each pixel to greyscale
-    # convert pixels to string of ASCII
+
+def to_grayscale(image):
+    return image.convert("L")
+
+
+def pixel_to_ascii(image):
+    pixels = image.getdata()
+    characters = "".join([ASCII_CHARS[pixel//25] for pixel in pixels])
+    return characters
+
+
+def main(new_width=100):
+
     path = input("Enter the pathname of the image: \n")
     try:
-        image = PIL.Image.open(path)
-    except:
+        image = Image.open(path)
+        filename = os.path.basename(path)
+        basename = os.path.splitext(filename)[0]
+
+    except FileNotFoundError:
         print("Pathname is invalid")
-    
-    return
+        exit()
+
+    # transform to string of ascii chars
+    new_image_data = pixel_to_ascii(to_grayscale(
+        resize_image(image, new_width)))
+
+    # format
+    pixel_count = len(new_image_data)
+    ascii_image = "\n".join([new_image_data[index:(index+new_width)]
+                             for index in range(0, pixel_count, new_width)])
+
+    # display
+    print(ascii_image)
+
+    # create new filepath
+    base_path = "C:/dev/marni-and-junimo/Marni-and-Junimo/Utilities/AsciiGenerator/image_result/"
+    new_filepath = base_path + "ascii_" + basename + ".txt"
+
+    # save
+    with open(new_filepath, "w+", encoding="utf-8") as file:
+        file.write(ascii_image)
+
 
 if __name__ == '__main__':
     main()
