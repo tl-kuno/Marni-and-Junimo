26fc32b5259b43cc3b951c29d8026fccee48049e
Taylor Kuno <kunot@oregonstate.edu>
2022-10-02 11:57:46 -0500

    fully commented, WIP: aspect ratio bug

--- Utilities/AsciiGenerator/AsciiGenerator.py
+++ Utilities/AsciiGenerator/AsciiGenerator.py
@@ -10,25 +10,57 @@ from PIL import Image
 
 ASCII_CHARS = ["@", "#", "S", "%", "?", "*", "+", " ", " ", " ", " "]
 
+# resize image according to a new width
+def resize_image(image, new_width=100):
+    """Resizes an image to the given width, maintaining aspect ratio.
 
-def resize_image(image, new_width):
+    Args:
+        image (PIL Image object): the image to be resized
+        new_width (int, optional): the width to which the image will be resized. Defaults to 100.
+
+    Returns:
+        PIL Image object: the resized image
+    """
     width, height = image.size
-    new_height = int(new_width * (height/width))
+    ratio = height/width
+    new_height = int(new_width * ratio)
     resized_image = image.resize((new_width, new_height))
-    return (resized_image)
+    return resized_image
 
-
+# switch the photo to grayscale
 def to_grayscale(image):
+    """Converts a given image to grayscale
+
+    Args:
+        image (PIL Image object): the image to be converted
+
+    Returns:
+        PIL Image object: the given image converted to grayscale
+    """
     return image.convert("L")
 
 
 def pixel_to_ascii(image):
+    """Interprets the intensity of each pixel and assigns an ASCII char with similar intensity.
+        Concatenates to an array of all ASCII chars.
+
+    Args:
+        image (PIL Image object): The image to be converted to ASCII characters
+
+    Returns:
+        array: array of ASCII chars corresponding to the pixels in the image
+    """
     pixels = image.getdata()
     characters = "".join([ASCII_CHARS[pixel//25] for pixel in pixels])
     return characters
 
 
 def main(new_width=100):
+    """Requests a filepath to an image from the user and converts the image to ASCII art.
+
+    Args:
+        new_width (int, optional): the desired width of the new image. Defaults to 100.
+    """
 
     path = input("Enter the pathname of the image: \n")
     try:
@@ -50,7 +82,7 @@ def main(new_width=100):
                              for index in range(0, pixel_count, new_width)])
 
     # display
-    print(ascii_image)
+    # print(ascii_image)
 
     # create new filepath
     base_path = "C:/dev/marni-and-junimo/Marni-and-Junimo/Utilities/AsciiGenerator/image_result/"
