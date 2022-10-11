766332c4da9cfe922273bc533a410f533b391ff6
Taylor Kuno <kunot@oregonstate.edu>
2022-10-11 15:09:19 -0500

    security issue bug

--- picnic-quest-ui/src/App.js
+++ picnic-quest-ui/src/App.js
@@ -3,15 +3,19 @@ import axios from 'axios'
 
 
 function App() {
+  
   const handleClick = () => {
     axios.get('http://picnic-quest.vercel.app/')
     .then( response => console.log("Hello World"))
   }
 
     return (
-      <div className='button__container'>
-        <button className='button' onClick={handleClick}>
-        Click Me</button>
+      <div>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/> 
+        <div className='button__container'>
+          <button className='button' onClick={handleClick}>
+          Click Me</button>
+        </div>
       </div>
     )
 }
