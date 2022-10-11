9c9caaf56e58f9ce6e9ca8c337d90986d2af856e
Taylor Kuno <kunot@oregonstate.edu>
2022-10-11 14:53:29 -0500

    testing serverless functions

--- picnic-quest-ui/src/App.js
+++ picnic-quest-ui/src/App.js
@@ -1,12 +1,30 @@
-import computer from './images/lofi-computer.png';
 import './App.css';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-        <img src={computer} alt="computer" />
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+       command: ''
+    }
+
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick () {
+    axios.get('http://picnic-quest.vercel.app/')
+    .then( response => console.log("Hello World"))
+  }
+
+  render () {
+    return (
+      <div className='button__container'>
+        <button className='button' onClick={this.handleClick}>
+        Click Me</button>
+      </div>
+    )
+  }
 }
 
 export default App;
