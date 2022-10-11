24dc5babd00a65de7fab52b0c004f99e15307b43
Taylor Kuno <kunot@oregonstate.edu>
2022-10-11 15:25:40 -0500

    change to fastapi library

--- picnic-quest-ui/api/index.py
+++ picnic-quest-ui/api/index.py
@@ -1,18 +1,7 @@
-from http.server import BaseHTTPRequestHandler
-from urllib import parse
+from fastapi import FastAPI
+app = FastAPI
 
 
-class handler(BaseHTTPRequestHandler):
-
-    def do_GET(self):
-        s = self.path
-        dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        if "name" in dic:
-            message = "Hello, " + dic["name"] + "!"
-        else:
-            message = "Hello, stranger!"
-        self.wfile.write(message.encode())
-        return
+@app.get("/")
+def read_root():
+    return {"message": "Server is up and running"}
