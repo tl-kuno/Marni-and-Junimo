0ef51a98f203da900b256e0d717600baa8b2436d
Taylor Kuno <kunot@oregonstate.edu>
2022-10-11 17:18:29 -0500

    shorten function

--- api/commands.py
+++ api/commands.py
@@ -1,5 +1,4 @@
 from http.server import BaseHTTPRequestHandler
-import logging
 
 
 class handler(BaseHTTPRequestHandler):
@@ -7,9 +6,7 @@ class handler(BaseHTTPRequestHandler):
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
-        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
-                     str(self.path), str(self.headers),
-                     post_data.decode('utf-8'))
+        print(post_data)
         self._set_response()
         self.wfile.write("POST request for {}"
                          .format(self.path).encode('utf-8'))
