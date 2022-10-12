1bc38fb75f9a4895ea3666d3c04314e8f8bbf2fa
Taylor Kuno <kunot@oregonstate.edu>
2022-10-12 09:14:36 -0500

    hello world api

--- api/commands.py
+++ api/commands.py
@@ -2,11 +2,9 @@ from http.server import BaseHTTPRequestHandler
 
 
 class handler(BaseHTTPRequestHandler):
-
-    def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content_length)
-        print(post_data)
-        self._set_response()
-        self.wfile.write("POST request for {}"
-                         .format(self.path).encode('utf-8'))
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(str('Hello World!!').encode())
+        return
