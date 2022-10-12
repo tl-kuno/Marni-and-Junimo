c9701566115cd67865e56e43e14cd34805e4203b
Alex Meyers <brumfial@oregonstate.edu>
2022-10-12 15:00:29 -0500

    Added Save and Load to character class. Split Item and Room into seperate files

---
+++ backend/Room.py
@@ -0,0 +1,50 @@
+class Room:
+    """
+    Creates a full room environment
+    """
+    def __init__(self, room_id, room_name, long_description,
+                 short_description, object_list, directions):
+        self.room_id = room_id
+        self.room_name = room_name
+        self.long_description = long_description
+        self.short_description = short_description
+        self.object_list = object_list
+        self.directions = directions  # [North, East, South, West]
+        self.visited = False        # Show full description only on first visit
+
+    def north(self):
+        """
+        Direction to the north, use in search for an entrance/exit
+        """
+        return self.directions[0]
+
+    def east(self):
+        """
+        Direction to the east, use in search for an entrance/exit
+        """
+        return self.directions[1]
+
+    def south(self):
+        """
+        Direction to the south, use in search for an entrance/exit
+        """
+        return self.directions[2]
+
+    def west(self):
+        """
+        Direction to the west, use in search for an entrance/exit
+        """
+        return self.directions[3]
+
+    def look(self):
+        """
+        repeats the long form explanation
+        """
+        print(self.long_description)
+
+    def __repr__(self):
+        """
+        Display the name of the room, exits in the room, and items in the room
+        """
+        return "\nName: {}, Exits: {}, Items: {}".format(
+            self.room_name, self.directions, self.object_list)
--- backend/main.py
+++ backend/main.py
@@ -1,79 +1,10 @@
+from Room import Room
+from Item import Item
+
+
 def test_function():
     sample = "I made it to the other file."
     return sample
-
-
-class Room:
-    """
-    Creates a full room environment
-    """
-    def __init__(self, room_id, room_name, long_description,
-                 short_description, object_list, directions):
-        self.room_id = room_id
-        self.room_name = room_name
-        self.long_description = long_description
-        self.short_description = short_description
-        self.object_list = object_list
-        self.directions = directions  # [North, East, South, West]
-        self.visited = False        # Show full description only on first visit
-
-    def north(self):
-        """
-        Direction to the north, use in search for an entrance/exit
-        """
-        return self.directions[0]
-
-    def east(self):
-        """
-        Direction to the east, use in search for an entrance/exit
-        """
-        return self.directions[1]
-
-    def south(self):
-        """
-        Direction to the south, use in search for an entrance/exit
-        """
-        return self.directions[2]
-
-    def west(self):
-        """
-        Direction to the west, use in search for an entrance/exit
-        """
-        return self.directions[3]
-
-    def look(self):
-        """
-        repeats the long form explanation
-        """
-        print(self.long_description)
-
-    def __repr__(self):
-        """
-        Display the name of the room, exits in the room, and items in the room
-        """
-        return "\nName: {}, Exits: {}, Items: {}".format(
-            self.room_name, self.directions, self.object_list)
-
-
-class Item:
-    """
-    Creates an item and how that item could be interacted with
-    """
-    def __init__(self, item_name, description, can_pick_up, can_drop):
-        self.item_name = item_name
-        self.description = description
-        self.can_pick_up = can_pick_up
-        self.can_drop = can_drop
-
-    def look_at(self):
-        """
-        Fictionally interesting explanation of feature or object
-        """
-        print(self.description)
-
-    def __repr__(self):
-        return self.item_name
-
 
 def main():
     """
@@ -85,7 +16,7 @@ def main():
     room_list = []
 
 # -----------------------------------------------------
-# -                      Items                        -
+# -                      Create Items                 -
 # -----------------------------------------------------
 
     # Item Name: Blueberries
@@ -144,7 +75,7 @@ def main():
     rocking_chair = Item("Rocking Chair", "Description", False, False)
 
 # -----------------------------------------------------
-# -                      Rooms                        -
+# -                     Create Rooms                  -
 # -----------------------------------------------------
 
     # Room ID: 0
