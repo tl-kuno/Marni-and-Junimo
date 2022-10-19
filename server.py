d188e0125a2cc8b33c672d8391abf6e8d77dcaf2
Taylor Kuno <kunot@oregonstate.edu>
2022-10-19 17:20:37 -0500

    cross origin import

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import request
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 import json
 # from main import test_function
 from character import Character
@@ -14,6 +14,7 @@ player = Character("Player 1", location=room_list[0])
 
 
 @app.route('/', methods=["GET", "POST"])
+@cross_origin(origin='*', headers=['Content-Type', 'application/json'])
 def handle_request():
     command = str(request.args.get('command'))
     output = check_and_move(command, player.location, room_list)
