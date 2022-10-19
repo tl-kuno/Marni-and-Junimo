7bfd7d7ed7b78d42084347877affde4aaf81060c
Taylor Kuno <kunot@oregonstate.edu>
2022-10-19 16:30:03 -0500

    prepped for trying character interaction

--- server.py
+++ server.py
@@ -2,23 +2,23 @@ from flask import Flask
 from flask import request
 from flask_cors import CORS
 import json
-from main import test_function
-# from backend.character import Character
-# from backend.main import check_and_move, init_room_list_and_items
+# from main import test_function
+from character import Character
+from main import check_and_move, init_room_list_and_items
 
 app = Flask(__name__)
 CORS(app)
 
-# room_list = init_room_list_and_items()
-# player = Character("Player 1", location=room_list[0])
+room_list = init_room_list_and_items()
+player = Character("Player 1", location=room_list[0])
 
 
 @app.route('/', methods=["GET", "POST"])
 def handle_request():
     command = str(request.args.get('command'))
-    # output = check_and_move(command, player.location, room_list)
-    # player.set_location(output)
-    output = test_function(command)
+    output = check_and_move(command, player.location, room_list)
+    player.set_location(output)
+    # output = test_function(command)
     data_set = {'output': output}
     json_dump = json.dumps(data_set)
     return json_dump
