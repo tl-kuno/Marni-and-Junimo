3ab6d3c9630afd7e294388989ffe7d727bd5b5ce
Taylor Kuno <kunot@oregonstate.edu>
2022-10-19 19:04:27 -0500

    updated messages and main for Kitchen

--- Messages.py
+++ Messages.py
@@ -1,4 +1,7 @@
 messages = {
     'kitchen': 
-        {'short': 'You find yourself in the brightly lit kitchen. To the North, you can see the pantry door. To the East, you can see the doorway to the living room.'}
+        {
+            'long': 'You stand in the brightly lit kitchen. The white tile floor glistens brightly, and rumbling sound of the running dishwasher fills the room. The humans must have been clumsy earlier, because a thick wooden ladle has rolled under the table in the center of the room. On the sleek marble countertops, you can see the corner of a plastic container of scrumptious, freshly picked blueberries! These would be perfect for a picnic!\nThe door to the pantry is ajar to the North of you.\nThe open doorway to the living room is to the East.\n',
+            'short':  'You find yourself in the brightly lit kitchen. \nTo the North, you can see the pantry door.\nTo the East, you can see the doorway to the living room.\n'  
+        }
     }
--- main.py
+++ main.py
@@ -193,15 +193,7 @@ def init_room_list_and_items():
     room_list.append(Room(
         2,
         "Kitchen",
-        "You stand in the brightly lit kitchen. The white tile floor glistens "
-        "brightly, and rumbling sound of the running dishwasher fills the "
-        "room. The humans must have been clumsy earlier, because a thick "
-        "wooden ladle has rolled under the table in the center of the room."
-        " On the sleek marble countertops, you can see the corner of a "
-        "plastic container of scrumptious, freshly picked blueberries! "
-        "These would be perfect for a picnic!\nThe door to the pantry is"
-        " ajar to the North of you.\nThe open doorway to the living room "
-        "is to the East.\n",
+        messages['kitchen']['long'],
         messages['kitchen']['short'],
         [wooden_spoon, blueberries],
         [3, 0, None, None]))
