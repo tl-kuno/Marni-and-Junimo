27308a20a80558b80d1caa106a9e9b8d61ddc6b8
Taylor Kuno <kunot@oregonstate.edu>
2022-10-21 13:25:54 -0500

    single folder structure

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import request
 from flask_cors import CORS
 import json
-import backend
+from main import handle_user_input
 
 app = Flask(__name__)
 CORS(app)
@@ -11,7 +11,7 @@ CORS(app)
 @app.route('/', methods=["GET", "POST"])
 def handle_request():
     command = str(request.args.get('command'))
-    output = backend.main.handle_user_input(command)
+    output = handle_user_input(command)
     data_set = {'output': output}
     json_dump = json.dumps(data_set)
     return json_dump
