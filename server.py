23cf4cbfef1c3e212157990cabbabe4391cb6c53
Taylor Kuno <kunot@oregonstate.edu>
2022-11-01 11:53:57 -0500

    added routing for newgame

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import request
 from flask_cors import CORS
 import json
-from main import handle_user_input
+from main import handle_user_input, newgame
 
 app = Flask(__name__)
 CORS(app)
@@ -17,16 +17,24 @@ def handle_interaction():
     return json_dump
 
 
+@app.route('/new', methods=["POST"])
+def handle_new_game():
+    data_set = {'confirmation': 'New game created'}
+    json_dump = json.dumps(data_set)
+    return json_dump
+
+
 @app.route('/save', methods=["POST"])
 def handle_save():
-    data_set = {'confirmation': 'Game Progress Saved'}
+    data_set = {'startDialogue': ''}
     json_dump = json.dumps(data_set)
     return json_dump
 
 
 @app.route('/load', methods=["GET"])
 def handle_load():
-    data_set = {'confirmation': 'Game Loaded from Last Save'}
+    intro = newgame()
+    data_set = {'intro': intro}
     json_dump = json.dumps(data_set)
     return json_dump
 
