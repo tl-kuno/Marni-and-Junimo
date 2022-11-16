33671ea093f187fa658ed835c9d131db3af71c98
Taylor Kuno <kunot@oregonstate.edu>
2022-11-16 11:29:01 -0600

    port instead of IP

--- server.py
+++ server.py
@@ -11,6 +11,7 @@ CORS(app)
 
 game_instances = {}
 
+
 # When a user lands on the page, the welcome message is sent
 @app.route('/start', methods=["POST"])
 def handle_start():
@@ -23,20 +24,21 @@ def handle_start():
 # return the current room and intro message
 @app.route('/new', methods=["POST"])
 def handle_new_game():
-    ip_address = str(request.remote_addr)
+    port = str(request.environ.get('REMOTE_PORT'))
     player = Character('Marni')
-    game_instances[ip_address] = player
-    intro = game_instances[ip_address].newgame(ip_address)
+    game_instances[port] = player
+    intro = game_instances[port].newgame(port)
     data_set = {'output': intro,
-                'location': game_instances[ip_address].location.room_name}
+                'location': game_instances[port].location.room_name}
     json_dump = json.dumps(data_set)
     return json_dump
+
 
 # When a user sends a command, 
 @app.route('/', methods=["GET", "POST"])
 def handle_interaction():
-    ip_address = str(request.remote_addr)
-    player = game_instances[ip_address]
+    port = str(request.environ.get('REMOTE_PORT'))
+    player = game_instances[port]
     command = str(request.args.get('command'))
     output = player.handle_user_input(command)
     data_set = {'output': output, 'location': player.location.room_name}
@@ -46,19 +48,19 @@ def handle_interaction():
 
 @app.route('/quit', methods=["POST"])
 def handle_quit_game():
-    ip_address = str(request.remote_addr)
-    player = game_instances[ip_address]
+    port = str(request.environ.get('REMOTE_PORT'))
+    player = game_instances[port]
     data_set = {'output': 'Game Over',
                 'location': player.location.room_name}
     json_dump = json.dumps(data_set)
-    del game_instances[ip_address]
+    del game_instances[port]
     return json_dump
 
 
 @app.route('/save', methods=["POST"])
 def handle_save():
-    ip_address = str(request.remote_addr)
-    player = game_instances[ip_address]
+    port = str(request.environ.get('REMOTE_PORT'))
+    player = game_instances[port]
     # output = player.savegame()
     data_set = {'output': 'Game Progress Saved',
                 'location': player.location.room_name}
@@ -68,8 +70,8 @@ def handle_save():
 
 @app.route('/load', methods=["GET"])
 def handle_load():
-    ip_address = str(request.remote_addr)
-    player = game_instances[ip_address]
+    port = str(request.environ.get('REMOTE_PORT'))
+    player = game_instances[port]
     # output = player.loadgame()
     data_set = {'output': 'Game Loaded from Last Save',
                 'location': player.location.room_name}
