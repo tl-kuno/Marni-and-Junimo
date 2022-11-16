166a50887d44d7f609dcf0e132d85b1515905d87
Taylor Kuno <kunot@oregonstate.edu>
2022-11-16 11:18:40 -0600

    attempt separate instances

--- server.py
+++ server.py
@@ -8,18 +8,10 @@ from messages import messages
 
 app = Flask(__name__)
 CORS(app)
-player = Character('Marni')
 
+game_instances = {}
 
-@app.route('/', methods=["GET", "POST"])
-def handle_interaction():
-    command = str(request.args.get('command'))
-    output = player.handle_user_input(command)
-    data_set = {'output': output, 'location': player.location.room_name}
-    json_dump = json.dumps(data_set)
-    return json_dump
-
-
+# When a user lands on the page, the welcome message is sent
 @app.route('/start', methods=["POST"])
 def handle_start():
     data_set = {'output': messages["welcome"]}
@@ -27,34 +19,60 @@ def handle_start():
     return json_dump
 
 
+# When a new game is started, create a game instance and store it in the dict
+# return the current room and intro message
 @app.route('/new', methods=["POST"])
 def handle_new_game():
     ip_address = str(request.remote_addr)
-    intro = player.newgame(ip_address)
-    data_set = {'output': intro, 'location': player.location.room_name}
+    player = Character('Marni')
+    game_instances[ip_address] = player
+    intro = game_instances[ip_address].newgame(ip_address)
+    data_set = {'output': intro,
+                'location': game_instances[ip_address].location.room_name}
+    json_dump = json.dumps(data_set)
+    return json_dump
+
+# When a user sends a command, 
+@app.route('/', methods=["GET", "POST"])
+def handle_interaction():
+    ip_address = str(request.remote_addr)
+    player = game_instances[ip_address]
+    command = str(request.args.get('command'))
+    output = player.handle_user_input(command)
+    data_set = {'output': output, 'location': player.location.room_name}
     json_dump = json.dumps(data_set)
     return json_dump
 
 
 @app.route('/quit', methods=["POST"])
 def handle_quit_game():
-    data_set = {'output': 'Game Over', 'location': player.location.room_name}
+    ip_address = str(request.remote_addr)
+    player = game_instances[ip_address]
+    data_set = {'output': 'Game Over',
+                'location': player.location.room_name}
     json_dump = json.dumps(data_set)
+    del game_instances[ip_address]
     return json_dump
 
 
 @app.route('/save', methods=["POST"])
 def handle_save():
+    ip_address = str(request.remote_addr)
+    player = game_instances[ip_address]
     # output = player.savegame()
-    data_set = {'output': 'Game Progress Saved', 'location': player.location.room_name}
+    data_set = {'output': 'Game Progress Saved',
+                'location': player.location.room_name}
     json_dump = json.dumps(data_set)
     return json_dump
 
 
 @app.route('/load', methods=["GET"])
 def handle_load():
+    ip_address = str(request.remote_addr)
+    player = game_instances[ip_address]
     # output = player.loadgame()
-    data_set = {'output': 'Game Loaded from Last Save', 'location': player.location.room_name}
+    data_set = {'output': 'Game Loaded from Last Save',
+                'location': player.location.room_name}
     json_dump = json.dumps(data_set)
     return json_dump
 
