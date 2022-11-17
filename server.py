4c5818483af61e517b9659d0ba2aa3029e35bec5
Taylor Kuno <kunot@oregonstate.edu>
2022-11-16 19:29:57 -0600

    doc string updates

--- server.py
+++ server.py
@@ -14,24 +14,39 @@ game_instances = {}
 
 
 def new_key():
+    """Return random number(1-100) that is not already a key in the dict"""
     key = str(random.randrange(101))
     while key in game_instances:
         key = str(random.randrange(101))
     return key
 
 
-# When a user lands on the page, the welcome message is sent
 @app.route('/start', methods=["POST"])
 def handle_start():
+    """
+    Summary:
+        When a user lands on the page, the welcome message is sent
+    Returns:
+        output: Junimo's welcome message to the user
+    """
     data_set = {'output': messages["welcome"]}
     json_dump = json.dumps(data_set)
     return json_dump
 
 
-# When a new game is started, create a game instance and store it in the dict
-# return the current room and intro message
 @app.route('/new', methods=["GET", "POST"])
 def handle_new_game():
+    """
+    Summary:
+        When a new game is started, create a game instance and store it
+        in the dict with a newly genereated num between 1-100 as the key
+        TODO    Consider other options for the key, such as a user given "name"
+                Benefits of this are making the load/save front end work better
+    Returns:
+        key: random int between 1-100 generated as a dictionary key
+        output: Starting room introduction/description
+        location: the current room that the player is located in
+    """
     key = new_key()
     player = Character("Marni")
     game_instances[key] = player
@@ -43,9 +58,20 @@ def handle_new_game():
     return json_dump
 
 
-# When a user sends a command,
 @app.route('/', methods=["GET", "POST"])
 def handle_interaction():
+    """
+    Summary:
+        When a user sends a command, use the proper Character class instance
+        from the dictionarry lookup and send back a response string/room
+    Params:
+        command: the string entered by the user
+        key: the dictionary key of the user's character class
+    Returns:
+        output: the game's response to the users command
+        location: the current room that the player is located in
+    """
+
     key = request.args.get('key')
     player = game_instances[key]
     command = str(request.args.get('command'))
@@ -56,30 +82,59 @@ def handle_interaction():
     return json_dump
 
 
+# TODO @ alex do you want Junimo to say something cuter than Game Over?
+# TODO, I will be attempting to call this function on window close as well
+# have to think about if a game has been saved and how to handle this
 @app.route('/quit', methods=["GET", "POST"])
 def handle_quit_game():
+    """
+    Summary:
+        When a user quits (clicking the button, closing the browser)
+        The information is removed from the array
+    Params:
+        key: the dictionary key of the user's character class
+    Returns:
+        output: Junimo's response to the end of game
+    """
     key = request.args.get('key')
-    player = game_instances[key]
-    data_set = {'output': 'Game Over',
-                'location': player.location.room_name}
+    data_set = {'output': 'Game Over'}
     json_dump = json.dumps(data_set)
     del game_instances[key]
     return json_dump
 
 
+# TODO what information (if any) do you need from me to be able to save
+# TODO I will have to look into having the user save a "name"
 @app.route('/save', methods=["GET", "POST"])
 def handle_save():
+    """
+    Summary:
+        When a user saves via the button
+    Params:
+        key: the dictionary key of the user's character class
+    Returns:
+        output: Junimo's response to the end of game
+    """
     key = request.args.get('key')
     player = game_instances[key]
     # output = player.savegame()
-    data_set = {'output': 'Game Progress Saved',
-                'location': player.location.room_name}
+    data_set = {'output': 'Game Progress Saved'}
     json_dump = json.dumps(data_set)
     return json_dump
 
 
+# TODO, need to think about the reverse of save
+# TODO have output return the long string from the current room
 @app.route('/load', methods=["GET", "POST"])
 def handle_load():
+    """
+    Summary:
+        When a user loads via the load button
+    Params:
+        key: the dictionary key of the user's character class
+    Returns:
+        output: Junimo's response to the end of game
+    """
     key = request.args.get('key')
     player = game_instances[key]
     # output = player.loadgame()
