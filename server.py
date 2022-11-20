5abc19560a41aeb0d4543455fbdc8b2205aaf904
Taylor Kuno <kunot@oregonstate.edu>
2022-11-20 17:44:31 -0600

    try to updated our json file

--- server.py
+++ server.py
@@ -119,7 +119,6 @@ def handle_quit_game():
     return json_dump
 
 
-
 # TODO what information (if any) do you need from me to be able to save
 # TODO I will have to look into having the user save a "name"
 @app.route('/save', methods=["GET"])
@@ -132,12 +131,15 @@ def handle_save():
     Returns:
         output: Junimo's response to the end of game
     """
-    # key = request.args.get('key')
-    # player = game_instances[key]
+    key = request.args.get('key')
+    player = game_instances[key]
+    player_save_data = json.dumps(player)
+    pq_data.update(player_save_data)
+
     # output = player.savegame()
-    data_set = {'output': 'Game Progress Saved'}
-    json_dump = json.dumps(data_set)
-    return json_dump
+    # data_set = {'output': 'Game Progress Saved'}
+    # json_dump = json.dumps(data_set)
+    # return json_dump
 
 
 # TODO, need to think about the reverse of save
