7962529b96296185d7a50e42a67b172659fcaa23
Armon Tavakoulnia <71794386+atavakoulnia@users.noreply.github.com>
2022-11-21 09:09:49 -0800

    Pickled Room Data
    - data is now loaded in

--- roomlist.py
+++ roomlist.py
@@ -1,127 +1,12 @@
-from room import Room
-from item import Item
-from feature import Feature
-from nav import Direction
-from messages import messages
 import pickle
+
 
 def init_room_list_and_items():
     # list of all the rooms
     room_list = []
 
-    # -----------------------------------------------------
-    # -                      Create Items                 -
-    # -----------------------------------------------------
-
-    # Item Name: Blueberries
-    blueberries = Item("blueberries",
-                       messages['blueberries'])
-
-    # Item Name: Mushrooms
-    mushrooms = Item("mushrooms",
-                     messages['mushrooms'])
-
-    # Item Name: Dog Treats
-    dog_treats = Item("dog treats",
-                      messages['dog treats'])
-
-    # Item Name: Towel
-    towel = Item("towel",
-                 messages['towel'])
-
-    # Item Name: Umbrella
-    umbrella = Item("umbrella",
-                    messages['umbrella'])
-
-    # Item Name: Letter
-    letter = Item("letter",
-                  messages['letter'])
-
-    # Item Name: Wooden Spoon
-    wooden_spoon = Item("wooden spoon",
-                        messages['wooden spoon'])
-
-    # Item Name: Football Helmet
-    football_helmet = Item("football helmet",
-                           messages['football helmet'])
-
-    # Item Name: Soap
-    soap = Item("soap",
-                messages['soap'])
-
-    # Item Name: Flashlight
-    flashlight = Item("flashlight",
-                      messages['flashlight'])
-
-    # -----------------------------------------------------
-    # -                      Create Features               -
-    # -----------------------------------------------------
-
-    # Feature Name: Sofa
-    sofa = Feature("sofa", messages['sofa'])
-
-    # Feature Name: TV
-    tv = Feature("tv", messages['tv'])
-
-    # Feature Name: Mouse
-    mouse = Feature("mouse", messages['mouse'])
-
-    # Feature Name: Suitcase
-    suitcase = Feature("suitcase", messages['suitcase'])
-
-    # Feature Name: Refrigerator
-    refrigerator = Feature("refrigerator", messages['refrigerator'])
-
-    # Feature Name: Ants
-    ants = Feature("ants", messages['ants'])
-
-    # Feature Name: shelves
-    shelves = Feature("shelves", messages['shelves'])
-
-    # Feature Name: Treat bin
-    cleaning_supplies = Feature("Cleaning Supplies",
-                                messages['cleaning supplies'])
-
-    # Feature Name: Blanket
-    blanket = Feature("blanket", messages['blanket'])
-
-    # Feature Name: window
-    window = Feature("window", messages['window'])
-
-    # Feature Name: sink
-    sink = Feature("sink", messages['sink'])
-
-    # Feature Name: bathtub
-    bathtub = Feature("bathtub", messages['bathtub'])
-
-    # Feature Name: Rocking Chair
-    rocking_chair = Feature("rocking chair", messages['rocking chair'])
-
-    # Feature Name: Dirt pile
-    dirt_pile = Feature("dirt pile", messages['dirt pile'])
-
-    # Feature Name: Raccoon
-    raccoon = Feature("raccoon", messages['raccoon'])
-
-    # Feature Name: Guitar
-    guitar = Feature("guitar", messages['guitar'])
-
-    # Feature Name: Birds
-    birds = Feature("birds", messages['birds'])
-
-    # Feature Name: Neighborhood
-    neighborhood = Feature("neighborhood", messages['neighborhood'])
-
-    # Feature Name: Table
-    table = Feature("table", messages['table'])
-
-    # Feature Name: Friends
-    # TODO write a func to give result based on collected items & animals??
-    friends = Feature("friends",
-                      "END GAME!")
-
     # # -----------------------------------------------------
-    # # -                     Create Rooms                  -
+    # # -                     Open Rooms                    -
     # # -----------------------------------------------------
 
     # # Room ID: 0
@@ -130,177 +15,81 @@ def init_room_list_and_items():
     # # Feature List: Sofa, TV
     # # Direction: [N: Basement, E: Bedroom , S: Porch, W: Kitchen]
     # # Custom Exit: smelly staircase
-    living_room = Room(
-        0,
-        "Living Room",
-        messages["living room.long"],
-        messages["living room.short"],
-        [flashlight, letter],
-        [sofa, tv],
-        [1, 4, 6, 2],
-        {
-            "creaky wooden stairs": Direction.NORTH,
-            "creaky stairs": Direction.NORTH,
-            "wooden stairs": Direction.NORTH,
-            "stairs": Direction.NORTH,
-            "creaky wooden staircase": Direction.NORTH,
-            "creaky staircase": Direction.NORTH,
-            "wooden staircase": Direction.NORTH,
-            "staircase": Direction.NORTH,
-            "kitchen": Direction.WEST,
-            "bedroom": Direction.EAST,
-            "basement": Direction.NORTH,
-            "porch": Direction.SOUTH
-        })
+    p_lr = open("rooms/living_room.p", "rb")
+    unpickled_living_room = pickle.load(p_lr)
+    p_lr.close()
 
     # # Room ID: 1
     # # Room: Basement
     # # Object List: Mushrooms
     # # Feature List: Mouse, Suitcase
     # # Direction: [N: None, E: None , S: Living Room, W: None]
-    basement = Room(
-        1,
-        "Basement",
-        messages["basement.long"],
-        messages["basement.short"],
-        [mushrooms],
-        [mouse, suitcase],
-        [None, None, 0, None],
-        {
-            "creaky wooden stairs": Direction.SOUTH,
-            "creaky stairs": Direction.SOUTH,
-            "wooden stairs": Direction.SOUTH,
-            "stairs": Direction.SOUTH,
-            "creaky wooden staircase": Direction.SOUTH,
-            "creaky staircase": Direction.SOUTH,
-            "wooden staircase": Direction.SOUTH,
-            "staircase": Direction.SOUTH,
-            "living room": Direction.SOUTH
-        })
+    p_base = open("rooms/basement.p", "rb")
+    unpickled_basement = pickle.load(p_base)
+    p_base.close()
 
     # # Room ID: 2
     # # Room: Kitchen
     # # Object List: Wooden Spoon, Blueberries
     # # Feature List: Refrigerator, Ants
     # # Direction: [N: Pantry, E: Living Room, S: None, W: None]
-    kitchen = Room(
-        2,
-        "Kitchen",
-        messages["kitchen.long"],
-        messages["kitchen.short"],
-        [wooden_spoon, blueberries],
-        [refrigerator, ants],
-        [3, 0, None, None],
-        {
-            "pantry": Direction.NORTH,
-            "living room": Direction.EAST
-        })
+    p_kit = open("rooms/kitchen.p", "rb")
+    unpicked_kitchen = pickle.load(p_kit)
+    p_kit.close()
 
     # # Room ID: 3
     # # Room: Pantry
     # # Object List: Dog Treats
     # # Feature List: Shelves, Cleaning Supplies
     # # Direction: [N: None, E: None, S: Kitchen, W: None]
-    pantry = Room(
-        3,
-        "Pantry",
-        messages["pantry.long"],
-        messages["pantry.short"],
-        [dog_treats],
-        [shelves, cleaning_supplies],
-        [None, None, 2, None],
-        {
-            "kitchen": Direction.SOUTH
-        })
+    p_pan = open("rooms/pantry.p", "rb")
+    unpicked_pantry = pickle.load(p_pan)
+    p_pan.close()
 
     # # Room ID: 4
     # # Room: Bedroom
     # # Object List: None
     # # Feature List: Blanket, Window
     # # Direction: [N: Bathroom, E: None, S: None, W: Living Room]
-    bedroom = Room(
-        4,
-        "Bedroom",
-        messages["bedroom.long"],
-        messages["bedroom.short"],
-        [],
-        [blanket, window],
-        [5, None, None, 0],
-        {
-            "living room": Direction.WEST,
-            "bathroom": Direction.NORTH
-        })
+    p_bed = open("rooms/bedroom.p", "rb")
+    unpickled_bedroom = pickle.load(p_bed)
+    p_bed.close()
 
     # # Room ID: 5
     # # Room: Bathroom
     # # Object List: Towel, Soap
     # # Feature List: Sink, Bathtub
     # # Direction: [N: None, E: None, S: Bedroom, W: None]
-    bathroom = Room(
-        5,
-        "Bathroom",
-        messages["bathroom.long"],
-        messages["bathroom.short"],
-        [towel, soap],
-        [sink, bathtub],
-        [None, None, 4, None],
-        {
-            "bedroom": Direction.SOUTH
-        })
+    p_bath = open("rooms/bathroom.p", "rb")
+    unpickled_bathroom = pickle.load(p_bath)
+    p_bath.close()
 
     # # Room ID: 6
     # # Room: Porch
     # # Object List: None
     # # Feature List: Rocking Chair, Dirt pile
     # # Direction: [N: Living Room, E: Park, S: Roof, W: Alley]
-    porch = Room(
-        6,
-        "Porch",
-        messages["porch.long"],
-        messages["porch.short"],
-        [],
-        [rocking_chair, dirt_pile],
-        [0, 9, 8, 7],
-        {
-            "living room": Direction.NORTH,
-            "park": Direction.EAST,
-            "roof": Direction.SOUTH,
-            "alley": Direction.WEST
-        })
+    p_por = open("rooms/porch.p", "rb")
+    unpickled_porch = pickle.load(p_por)
+    p_por.close()
 
     # # Room ID: 7
     # # Room: Alley
     # # Object List: Umbrella, Football Helmet
     # # Feature List: Raccoon, Guitar
     # # Direction: [N: None, E: Porch, S: None, W: None]
-    alley = Room(
-        7,
-        "Alley",
-        messages["alley.long"],
-        messages["alley.short"],
-        [umbrella, football_helmet],
-        [raccoon, guitar],
-        [None, 6, None, None],
-        {
-            "porch": Direction.EAST
-        })
+    p_all = open("rooms/alley.p", "rb")
+    unpickled_alley = pickle.load(p_all)
+    p_all.close()
 
     # # Room ID: 8
     # # Room: Roof
     # # Object List: None
     # # Feature List: Birds, neighborhood
     # # Direction: [N: Porch, E: None, S: None, W: None]
-    roof = Room(
-        8,
-        "Roof",
-        messages["roof.long"],
-        messages["roof.short"],
-        [],
-        [birds, neighborhood],
-        [6, None, None, None],
-        {
-            "porch": Direction.NORTH
-        })
+    p_roof = open("rooms/roof.p", "rb")
+    unpickled_roof = pickle.load(p_roof)
+    p_roof.close()
 
     # # Room ID: 9
     # # Room: Park
@@ -308,72 +97,23 @@ def init_room_list_and_items():
     # # Feature List: Table, Friends
     # # Direction: [N: None, E: None, S: None, W: Porch]
     # # TODO: implement a final game state, based on items acquired
-    park = Room(
-        9,
-        "Park",
-        messages["park.long"],
-        messages["park.short"],
-        [],
-        [table, friends],
-        [None, None, None, 6])
+    p_park = open("rooms/park.p", "rb")
+    unpickled_park = pickle.load(p_park)
+    p_park.close()
 
-    # ------
+    # # -----------------------------------------------------
+    # # -              Append Room Lists                    -
+    # # -----------------------------------------------------
 
-    p_lr = open("rooms/living_room.p", "wb")
-    p_base = open("rooms/basement.p", "wb")
-    p_kit = open("rooms/kitchen.p", "wb")
-    p_pan = open("rooms/pantry.p", "wb")
-    p_bed = open("rooms/bedroom.p", "wb")
-    p_bath = open("rooms/bathroom.p", "wb")
-    p_por = open("rooms/porch.p", "wb")
-    p_all = open("rooms/alley.p", "wb")
-    p_roof = open("rooms/roof.p", "wb")
-    p_park = open("rooms/park.p", "wb")
+    room_list.append(unpickled_living_room)
+    room_list.append(unpickled_basement)
+    room_list.append(unpicked_kitchen)
+    room_list.append(unpicked_pantry)
+    room_list.append(unpickled_bedroom)
+    room_list.append(unpickled_bathroom)
+    room_list.append(unpickled_porch)
+    room_list.append(unpickled_alley)
+    room_list.append(unpickled_roof)
+    room_list.append(unpickled_park)
 
-    pickle.dump(living_room, p_lr)
-    pickle.dump(basement, p_base)
-    pickle.dump(kitchen, p_kit)
-    pickle.dump(pantry, p_pan)
-    pickle.dump(bedroom, p_bed)
-    pickle.dump(bathroom, p_bath)
-    pickle.dump(porch, p_por)
-    pickle.dump(alley, p_all)
-    pickle.dump(roof, p_roof)
-    pickle.dump(park, p_park)
-
-    # p_lr = open("rooms/living_room.p",'rb')
-    # p_base = open("rooms/basement.p",'rb')
-    # p_kit = open("rooms/kitchen.p",'rb')
-    # p_pan = open("rooms/pantry.p", "rb")
-    # p_bed = open("rooms/bedroom.p", "rb")
-    # p_bath = open("rooms/bathroom.p", "rb")
-    # p_por = open("rooms/porch.p", "rb")
-    # p_all = open("rooms/alley.p", "rb")
-    # p_roof = open("rooms/roof.p", "rb")
-    # p_park = open("rooms/park.p", "rb")
-
-    # unpickled_living_room = pickle.load(p_lr)
-    # unpickled_basement = pickle.load(p_base)
-    # unpicked_kitchen = pickle.load(p_kit)
-    # unpicked_pantry = pickle.load(p_pan)
-    # unpickled_bedroom = pickle.load(p_bed)
-    # unpickled_bathroom = pickle.load(p_bath)
-    # unpickled_porch = pickle.load(p_por)
-    # unpickled_alley = pickle.load(p_all)
-    # unpickled_roof = pickle.load(p_roof)
-    # unpickled_park = pickle.load(p_park)
-
-    # room_list.append(unpickled_living_room)
-    # room_list.append(unpickled_basement)
-    # room_list.append(unpicked_kitchen)
-    # room_list.append(unpicked_pantry)
-    # room_list.append(unpickled_bedroom)
-    # room_list.append(unpickled_bathroom)
-    # room_list.append(unpickled_porch)
-    # room_list.append(unpickled_alley)
-    # room_list.append(unpickled_roof)
-    # room_list.append(unpickled_park)
-
-    # print(room_list)
-
-init_room_list_and_items()
+    return room_list
